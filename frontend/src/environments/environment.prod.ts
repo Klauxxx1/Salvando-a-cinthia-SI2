5badf2c5bff050b3dae716aa260565ed14f42539
Klaus Parada <klausparada056@gmail.com>
2026-04-29 17:12:38 -0400

    Add Docker setup for prod

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: '',
+  firebase: {
+    apiKey: "AIzaSyCPKP7fCGWhuU7FshRaaals1cFTQOOwN4g",
+    authDomain: "auxiliomecanico-f0789.firebaseapp.com",
+    projectId: "auxiliomecanico-f0789",
+    storageBucket: "auxiliomecanico-f0789.firebasestorage.app",
+    messagingSenderId: "314510612181",
+    appId: "1:314510612181:web:f747d3182a922ca9ffb45c",
+    measurementId: "G-R7MDFJRYQ6"
+  },
+  firebaseVapidKey: "BNS653N8V9W1yoUjfLifFQoXyfaJxKIMBAdTl0FHaVc7Dl8IeMlLaWfMTEnaSvWDmGBnu2Q-x-ZJLir9om2KrIc"
+};
